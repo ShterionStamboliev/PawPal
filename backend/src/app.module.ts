b16fa780f6f07ea0e468f08ff1e72460c0248bca
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-12-29 22:16:07 +0200

    added auth modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,6 +7,7 @@ import { PetModule } from './pet/pet.module';
 import { UserModule } from './user/user.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -29,6 +30,7 @@ import { APP_GUARD } from '@nestjs/core';
         MongooseModule.forRoot(process.env.DB_URI),
         UserModule,
         PetModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [
