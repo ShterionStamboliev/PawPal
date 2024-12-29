025d7f0e9084ef42d945dcd67c8dd4a4a3cc1a1d
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-12-29 23:09:22 +0200

    implemented user authentication

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/user/schemas/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dto/signUp.dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectModel(User.name)
+        private userModel: Model<User>,
+        private jwtService: JwtService,
+    ) {}
+
+    async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+        const {
+            firstName,
+            lastName,
+            email,
+            address: { city, phone },
+            password,
+        } = signUpDto;
+
+        console.log(firstName, lastName, email, city, phone, password);
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await this.userModel.create({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            address: {
+                city,
+                phone,
+            },
+        });
+
+        const token = this.jwtService.sign({ id: user._id });
+
+        return { token };
+    }
+}
---
+++ backend/src/auth/dto/signUp.dto.ts
@@ -0,0 +1,34 @@
+import { Type } from 'class-transformer';
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsObject,
+    IsString,
+    MinLength,
+    ValidateNested,
+} from 'class-validator';
+import { AddressDto } from 'src/user/dto/create-user.dto';
+
+export class SignUpDto {
+    @IsString({ message: 'First name must be text' })
+    @IsNotEmpty({ message: 'First name is required' })
+    readonly firstName: string;
+
+    @IsString({ message: 'Last name must be text' })
+    @IsNotEmpty({ message: 'Last name is required' })
+    readonly lastName: string;
+
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
+    readonly email: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Password is required' })
+    @MinLength(6, { message: 'Password must be at least 6 characters' })
+    readonly password: string;
+
+    @IsObject()
+    @ValidateNested()
+    @Type(() => AddressDto)
+    readonly address: AddressDto;
+}
