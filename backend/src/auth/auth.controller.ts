9bfd1635e428906f12bb514a5bf06272f3c91932
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-12-29 23:16:13 +0200

    added signin/signup methods

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signUp.dto';
+import { SignInDto } from './dto/signIn.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
     @Post('signup')
     signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
         return this.authService.signUp(signUpDto);
+    }
+
+    @Get('signin')
+    signIn(@Body() signInDto: SignInDto): Promise<{ token: string }> {
+        return this.authService.signIn(signInDto);
     }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema';
 import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from './dto/signUp.dto';
 import * as bcrypt from 'bcrypt';
+import { SignInDto } from './dto/signIn.dto';
 @Injectable()
 export class AuthService {
     constructor(
@@ -36,6 +37,26 @@ export class AuthService {
                 phone,
             },
         });
+
+        const token = this.jwtService.sign({ id: user._id });
+
+        return { token };
+    }
+
+    async signIn(signInDto: SignInDto): Promise<{ token: string }> {
+        const { email, password } = signInDto;
+
+        const user = await this.userModel.findOne({ email });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
 
         const token = this.jwtService.sign({ id: user._id });
 
