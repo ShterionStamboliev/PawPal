d85922cdf0192dfc29cffd73b22754f0896b1c87
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2025-01-05 15:44:36 +0200

    refactor: simplify navigation links in Header component using an array mapping

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,6 +2,12 @@ import paw from '@/assets/cat-paw.png';
 import { Button } from '../ui/button';
 import { Link, NavLink } from 'react-router';
 
+const navLinks = [
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+    { name: 'Contacts', path: '/contacts' },
+];
+
 const Header = () => {
     return (
         <header className='bg-orange-200 shadow-orange-200 shadow-sm fixed top-0 left-0 right-0 z-50'>
@@ -17,36 +23,19 @@ const Header = () => {
                     </h1>
                 </div>
                 <nav className='flex gap-12 items-center'>
-                    <NavLink
-                        to='/'
-                        className={({ isActive }) =>
-                            isActive
-                                ? 'text-2xl font-chewy text-red-400'
-                                : 'text-2xl font-chewy text-red-300 hover:text-red-400'
-                        }
-                    >
-                        Home
-                    </NavLink>
-                    <NavLink
-                        to='/about'
-                        className={({ isActive }) =>
-                            isActive
-                                ? 'text-2xl font-chewy text-red-400'
-                                : 'text-2xl font-chewy text-red-300 hover:text-red-400'
-                        }
-                    >
-                        About
-                    </NavLink>
-                    <NavLink
-                        to='/contacts'
-                        className={({ isActive }) =>
-                            isActive
-                                ? 'text-2xl font-chewy text-red-400'
-                                : 'text-2xl font-chewy text-red-300 hover:text-red-400'
-                        }
-                    >
-                        Contacts
-                    </NavLink>
+                    {navLinks.map((link) => (
+                        <NavLink
+                            to={link.path}
+                            key={link.name}
+                            className={({ isActive }) =>
+                                isActive
+                                    ? 'text-2xl font-chewy text-red-400'
+                                    : 'text-2xl font-chewy text-red-300 hover:text-red-400'
+                            }
+                        >
+                            {link.name}
+                        </NavLink>
+                    ))}
                 </nav>
                 <Button className='bg-red-400 font-semibold font-manrope hover:bg-red-300'>
                     <img src={paw} alt='Cat Paw' className='h-6 w-6' />
