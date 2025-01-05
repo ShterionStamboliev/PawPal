9058eeb254e35036f883f98121efba7aedefd026
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2025-01-05 15:38:52 +0200

    feat: enhance Header and Hero components with improved layout and animations

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,16 +5,18 @@ import { Link, NavLink } from 'react-router';
 const Header = () => {
     return (
         <header className='bg-orange-200 shadow-orange-200 shadow-sm fixed top-0 left-0 right-0 z-50'>
-            <div className='container flex mx-auto p-4 justify-between items-center'>
+            <div className='flex mx-auto px-2 md:px-12 py-4 justify-between items-center'>
                 <div className='flex items-center gap-2 flex-shrink-0'>
                     <img
                         src={paw}
                         alt='Cat Paw'
                         className='h-14 w-14 -rotate-12'
                     />
-                    <h1 className='font-modak text-4xl text-red-400'>PawPal</h1>
+                    <h1 className='hidden md:inline-flex font-modak text-4xl text-red-400'>
+                        PawPal
+                    </h1>
                 </div>
-                <nav className='flex gap-4 items-center'>
+                <nav className='flex gap-12 items-center'>
                     <NavLink
                         to='/'
                         className={({ isActive }) =>
--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,11 +1,13 @@
 import dog from '@/assets/happy-dog.png';
+import { motion } from 'framer-motion';
+import { Button } from '../ui/button';
 
 const Hero = () => {
     return (
         <section className='bg-yellow-50 w-[100vw]'>
-            <div className='flex flex-col md:flex-row mx-auto p-4 items-center justify-between gap-2'>
-                <div className='text-center max-w-1xl'>
-                    <h1 className='font-manrope tracking-wider font-bold text-5xl leading-relaxed'>
+            <div className='flex flex-col md:flex-row mx-auto py-4 justify-between'>
+                <div className='text-center md:text-left px-8 md:px-12 pb-4 md:p-12'>
+                    <h1 className='font-manrope md:w-2/3 tracking-wider font-bold text-5xl leading-relaxed'>
                         For{' '}
                         <span className='font-chewy text-red-400'>
                             pawsitively
@@ -16,14 +18,26 @@ const Hero = () => {
                         </span>
                         Your pet's best friend 🐾
                     </h1>
+                    <h2 className='font-manrope text-2xl md:w-2/3 mt-4'>
+                        We provide the best medical treatment, examination,
+                        grooming and general health care of your pets.
+                    </h2>
+                    <Button className='bg-red-400 mt-10 p-8 text-lg font-semibold font-manrope hover:bg-red-300'>
+                        Try our best foods now
+                    </Button>
                 </div>
-                <div className='flex-shrink-0 w-full md:w-1/2'>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    className='flex-shrink-0 w-full px-4 md:px-0 md:pr-12 md:w-1/2'
+                >
                     <img
                         src={dog}
                         alt='Dog'
                         className='w-full md:max-w-full rounded-lg'
                     />
-                </div>
+                </motion.div>
             </div>
         </section>
     );
