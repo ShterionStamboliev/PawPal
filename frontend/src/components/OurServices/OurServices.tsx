9901ccf99cf511b42e75f3325f37c1eef78cc61c
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2025-01-08 17:16:26 +0200

    refactor OurServices component and add ServicesCarousel for improved structure and functionality

--- frontend/src/components/OurServices/OurServices.tsx
+++ frontend/src/components/OurServices/OurServices.tsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '../ui/card';
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from '../ui/carousel';
 import { Separator } from '../ui/separator';
 import Autoplay from 'embla-carousel-autoplay';
+import ServicesCarousel from './ServicesCarousel';
+import { useRef } from 'react';
 
 const OurServices = () => {
-    const plugin = React.useRef(
-        Autoplay({ delay: 3000, stopOnInteraction: true }),
-    );
+    const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: true }));
 
     return (
         <section className='flex flex-col items-center justify-center bg-orange-200/50'>
@@ -31,49 +16,7 @@ const OurServices = () => {
                     <Separator className='bg-red-200' />
                 </div>
             </div>
-            <Carousel
-                plugins={[plugin.current]}
-                onMouseEnter={() => plugin.current.stop()}
-                onMouseLeave={() => plugin.current.play()}
-                opts={{
-                    align: 'center',
-                }}
-                orientation='horizontal'
-                className='w-full md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl pb-8'
-            >
-                <CarouselContent>
-                    {Array.from({ length: 5 }).map((_, index) => (
-                        <CarouselItem
-                            key={index}
-                            className='sm:basis-1/3 md:basis-1/3 lg:basis-1/3'
-                        >
-                            <div className='p-1'>
-                                <Card>
-                                    <CardHeader className='p-0 space-y-0 relative'>
-                                        <CardTitle className='text-2xl absolute top-4 left-4'>
-                                            Echography
-                                        </CardTitle>
-                                        <CardDescription>
-                                            <img
-                                                src='https://via.placeholder.com/150'
-                                                alt='Service'
-                                                className='w-full h-32 rounded-t-md object-cover'
-                                            />
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <CardContent className='flex items-center justify-center p-6'>
-                                        <span className='text-3xl font-semibold'>
-                                            {index + 1}
-                                        </span>
-                                    </CardContent>
-                                </Card>
-                            </div>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext className='hidden md:inline-flex' />
-            </Carousel>
+            <ServicesCarousel plugin={plugin} />
         </section>
     );
 };
---
+++ frontend/src/components/OurServices/ServicesCarousel.tsx
@@ -0,0 +1,108 @@
+import dogWithBone from '@/assets/dog-with-bone.png';
+import dogWithFood from '@/assets/dog-with-food.png';
+import dogGrooming from '@/assets/dog-grooming.png';
+import { AutoplayType } from 'embla-carousel-autoplay';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '../ui/card';
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from '../ui/carousel';
+import { Button } from '../ui/button';
+
+type PetService = {
+    name: string;
+    image: string;
+    description?: string;
+    text?: string;
+};
+
+const petServices: PetService[] = [
+    {
+        name: 'Dental Care',
+        image: dogWithBone as string,
+        description: 'Dental care is important for your pet.',
+        text: "Maintaining your pet's oral hygiene is crucial for their overall health. Our dental care services include teeth cleaning, polishing, and treatment for gum diseases to keep their smiles healthy and bright.",
+    },
+    {
+        name: 'Nutrition',
+        image: dogWithFood as string,
+        description: 'Proper nutrition is essential for your pet.',
+        text: "A balanced diet is the foundation of your pet's health and well-being. Our nutrition counseling helps you choose the right food and feeding plans tailored to your pet's age, breed, and lifestyle.",
+    },
+    {
+        name: 'Grooming',
+        image: dogGrooming as string,
+        description: 'Grooming is important for your pet.',
+        text: 'Regular grooming keeps your pet looking and feeling their best. Grooming also helps detect skin conditions early, ensuring a healthy coat and skin.',
+    },
+];
+
+type ServicesCarouselProps = { plugin: React.MutableRefObject<AutoplayType> };
+
+const ServicesCarousel = ({ plugin }: ServicesCarouselProps) => {
+    return (
+        <Carousel
+            plugins={[plugin.current]}
+            onMouseEnter={() => plugin.current.stop()}
+            onMouseLeave={() => plugin.current.play()}
+            opts={{
+                align: 'center',
+            }}
+            orientation='horizontal'
+            className='w-full md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl pb-8'
+        >
+            <CarouselContent>
+                {petServices.map((service, index) => (
+                    <CarouselItem
+                        key={index}
+                        className='sm:basis-1/2 md:basis-1/2 lg:basis-1/2 xl:basis-1/3'
+                    >
+                        <div className='flex p-4 items-center justify-center'>
+                            <Card className='border-none sm:max-w-[22rem]'>
+                                <CardHeader className='p-0 space-y-0 relative shadow-md'>
+                                    <CardTitle className='text-2xl text-white/90 absolute top-4 left-4'>
+                                        {service.name}
+                                    </CardTitle>
+                                    <CardDescription className='rounded-t-lg'>
+                                        <img
+                                            src={service.image}
+                                            alt='Service'
+                                            className='w-full h-64 rounded-t-md'
+                                        />
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent className='flex space-y-4 shadow-md flex-col p-4 justify-center rounded-b-lg bg-gray-200/50'>
+                                    <p className='text-lg tracking-wide text-red-400 items-start font-manrope font-semibold'>
+                                        {service.description}
+                                    </p>
+                                    <p className='text-red-400/90'>
+                                        {service.text}
+                                    </p>
+                                    <Button className='bg-red-400 hover:bg-red-400/70'>
+                                        <p className='text-bold font-manrope text-white'>
+                                            Learn more
+                                        </p>
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext className='hidden md:inline-flex' />
+        </Carousel>
+    );
+};
+
+export default ServicesCarousel;
