eb509809ada9f7b9785a1b6d600536a713b0c985
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2025-01-21 13:34:23 +0200

    feat: refactor DashboardHeader and DashboardNavigation components for cleaner code and improved styling

--- frontend/src/components/UserDashboard/DashboardHeader.tsx
+++ frontend/src/components/UserDashboard/DashboardHeader.tsx
@@ -12,15 +12,13 @@ import HeaderUser from '../Header/HeaderUser';
 import HeaderUserNavigation from '../Header/HeaderUserNavigation';
 import { useAuthHandlers } from '@/hooks/mutations/mutation-handlers/useAuthHandlers';
 
-type DashboardHeaderProps = {};
-
-const DashboardHeader = ({}: DashboardHeaderProps) => {
+const DashboardHeader = () => {
     const { isAuthenticated } = useAuth();
 
     const { handleSignOutSubmit } = useAuthHandlers();
 
     return (
-        <header className='bg-rose-200/90 shadow-rose-300 shadow-sm fixed top-0 left-0 right-0 z-50'>
+        <header className='bg-gray-100/50 shadow-gray-300 shadow-sm fixed top-0 left-0 right-0 z-50'>
             <div className='flex mx-auto px-8 md:px-12 py-5 justify-between items-center backdrop-blur-sm backdrop-brightness-90'>
                 <HeaderMainTitle paw={paw} />
                 {isAuthenticated && (
--- frontend/src/components/UserDashboard/DashboardNavigation.tsx
+++ frontend/src/components/UserDashboard/DashboardNavigation.tsx
@@ -2,28 +2,26 @@ import { dashboardNav } from '@/constants/navigationLinks';
 import { User } from 'lucide-react';
 import { NavLink } from 'react-router';
 
-type DashboardNavigationProps = {};
-
-const DashboardNavigation = ({}: DashboardNavigationProps) => {
+const DashboardNavigation = () => {
     return (
         <div className='max-w-[15rem]'>
             <ul className='fixed flex flex-col border bg-white border-white shadow-[_0px_5px_10px_1px_rgba(0,0,0,0.3)] rounded-lg gap-8 text-start pt-10 h-[calc(100vh-9.5rem)] min-w-[15rem] items-start font-manrope'>
                 {dashboardNav.map((link, idx) => (
-                    <li key={idx} className='w-full flex'>
+                    <li key={idx} className='w-full h-8 flex items-center'>
+                        <div className='flex gap-2 ml-10'>
+                            <div className='flex flex-col'>
+                                <User size={20} />
+                            </div>
+                        </div>
                         <NavLink
                             to={link.path}
                             className={({ isActive }) =>
                                 isActive
-                                    ? 'text-lg font-manrope border-rose-700 text-rose-600 border-l-2'
-                                    : 'text-lg font-manrope transition ease-in-out duration-150'
+                                    ? 'text-sm font-manrope tracking-wide w-full border-rose-700 border-r-2 text-rose-600 font-semibold'
+                                    : 'text-sm font-manrope tracking-wide w-full text-rose-900 hover:text-rose-600 text-gray-500/80 transition ease-in-out duration-150'
                             }
                         >
-                            <div className='flex items-start gap-2 ml-10'>
-                                <div className='flex flex-col items-center'>
-                                    <User size={24} />
-                                </div>
-                                <span>{link.name}</span>
-                            </div>
+                            <span className='pl-3'>{link.name}</span>
                         </NavLink>
                     </li>
                 ))}
