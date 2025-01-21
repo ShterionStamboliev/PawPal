617748cf843104bc5cc36f08728b1c1e74bbfa15
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2025-01-21 16:35:12 +0200

    feat: update DashboardNavigation component layout and styling for improved user experience

--- frontend/src/components/UserDashboard/DashboardNavigation.tsx
+++ frontend/src/components/UserDashboard/DashboardNavigation.tsx
@@ -4,10 +4,13 @@ import { NavLink } from 'react-router';
 
 const DashboardNavigation = () => {
     return (
-        <div className='max-w-[15rem]'>
-            <ul className='fixed flex flex-col border bg-white border-white shadow-[_0px_5px_10px_1px_rgba(0,0,0,0.3)] rounded-lg gap-8 text-start pt-10 h-[calc(100vh-9.5rem)] min-w-[15rem] items-start font-manrope'>
+        <div className='max-w-[12rem]'>
+            <ul className='fixed flex flex-col border bg-white border-white shadow-[_0px_5px_10px_1px_rgba(0,0,0,0.3)] rounded-lg gap-2 text-start pt-10 h-[calc(100vh-9.5rem)] min-w-[12rem] items-start font-manrope'>
                 {dashboardNav.map((link, idx) => (
-                    <li key={idx} className='w-full h-8 flex items-center'>
+                    <li
+                        key={idx}
+                        className='w-full h-8 flex rounded-lg items-center hover:bg-rose-400 transition-colors duration-200 ease-in-out'
+                    >
                         <div className='flex gap-2 ml-10'>
                             <div className='flex flex-col'>
                                 <User size={20} />
@@ -18,7 +21,7 @@ const DashboardNavigation = () => {
                             className={({ isActive }) =>
                                 isActive
                                     ? 'text-sm font-manrope tracking-wide w-full border-rose-700 border-r-2 text-rose-600 font-semibold'
-                                    : 'text-sm font-manrope tracking-wide w-full text-rose-900 hover:text-rose-600 text-gray-500/80 transition ease-in-out duration-150'
+                                    : 'text-sm font-manrope tracking-wide w-full text-rose-900 hover:text-white text-gray-500/80 transition ease-in-out duration-150'
                             }
                         >
                             <span className='pl-3'>{link.name}</span>
