f55fea7491f49207a36e53c4fda1a047f4b15ea8
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2025-02-11 16:59:52 +0200

    implemented react-dropzone component for image upload

--- frontend/src/components/UserDashboard/Pet/Dropzone.tsx
+++ frontend/src/components/UserDashboard/Pet/Dropzone.tsx
@@ -1,69 +1,78 @@
-// import Dropzone, { useDropzone } from 'react-dropzone';
-// import { useState } from 'react';
-// import { Controller, useFormContext } from 'react-hook-form';
-// import {
-//     FormControl,
-//     FormField,
-//     FormItem,
-//     FormMessage,
-// } from '@/components/ui/form';
-// import { Card } from '@/components/ui/card';
-// import { UploadCloud } from 'lucide-react';
-// import { cn } from '@/lib/utils';
+import Dropzone from 'react-dropzone';
+import { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { FormField, FormMessage } from '@/components/ui/form';
+import { cn } from '@/lib/utils';
+import { usePetHook } from '@/hooks/forms/pet/usePetHook';
+import { Cloud } from 'lucide-react';
 
-// type DropzoneInputProps = {
-//     name: string;
-// };
+type DropzoneInputProps = {
+    name: string;
+    petId: string;
+};
 
-// const DropzoneInput = ({ name }: DropzoneInputProps) => {
-//     const { control } = useFormContext();
-//     const [preview, setPreview] = useState<string | null>(null);
+const DropzoneInput = ({ name, petId }: DropzoneInputProps) => {
+    const { control } = useFormContext();
+    const [preview, setPreview] = useState<string | null>(null);
 
-//     return (
-//         <FormField
-//             control={control}
-//             name={name}
-//             render={() => (
-//                 <Dropzone
-//                     accept={{
-//                         'image/*': ['.jpg', '.jpeg', '.png'],
-//                     }}
-//                     onDropAccepted={(acceptedFiles) => {
-//                         acceptedFiles.map((acceptedFile) => {
-//                             return {
-//                                 file: acceptedFile,
-//                                 name: '',
-//                                 description: '',
-//                                 price: 0,
-//                             };
-//                         });
-//                     }}
-//                     multiple={true}
-//                     maxSize={5000000}
-//                 >
-//                     {({ getRootProps, getInputProps }) => (
-//                         <div
-//                             {...getRootProps({
-//                                 className: cn(
-//                                     'p-3 mb-4 flex flex-col items-center justify-center w-full rounded-md cursor-pointer border border-[#e2e8f0]',
-//                                 ),
-//                             })}
-//                         >
-//                             <div className='flex items-center gap-x-3 mt-2 mb-2'>
-//                                 <label
-//                                     htmlFor='Products'
-//                                     className={`text-sm text-[7E8DA0] cursor-pointer focus:outline-none focus:underline`}
-//                                 >
-//                                     Add your Product Images
-//                                     <input {...getInputProps()} />
-//                                 </label>
-//                             </div>
-//                         </div>
-//                     )}
-//                 </Dropzone>
-//             )}
-//         />
-//     );
-// };
+    const { useUploadPetImage } = usePetHook();
 
-// export default DropzoneInput;
+    const uploadImage = useUploadPetImage(petId);
+
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field: { onChange } }) => (
+                <>
+                    <Dropzone
+                        accept={{ 'image/*': ['.jpg', '.jpeg', '.png'] }}
+                        onDropAccepted={(acceptedFiles) => {
+                            if (acceptedFiles.length > 0) {
+                                const file = acceptedFiles[0];
+                                uploadImage.mutate(file, {
+                                    onSuccess: (image) => {
+                                        onChange(image);
+                                        setPreview(URL.createObjectURL(file));
+                                    },
+                                });
+                            }
+                        }}
+                        multiple={false}
+                        maxSize={5000000}
+                    >
+                        {({ getRootProps, getInputProps }) => (
+                            <div
+                                {...getRootProps({
+                                    className: cn(
+                                        'p-3 mb-4 flex flex-col items-center justify-center w-full rounded-md cursor-pointer border',
+                                    ),
+                                })}
+                            >
+                                <input {...getInputProps()} />
+                                <div className='flex items-center gap-x-3 mt-2 mb-2'>
+                                    {preview ? (
+                                        <img
+                                            src={preview}
+                                            alt='Preview'
+                                            className='h-20 w-auto object-contain'
+                                        />
+                                    ) : (
+                                        <span className='flex flex-col text-sm items-center justify-center'>
+                                            <Cloud className='' />
+                                            Drag & drop an image of your pet, or
+                                            click here to select one
+                                        </span>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    </Dropzone>
+                    <FormMessage />
+                </>
+            )}
+        />
+    );
+};
+
+export default DropzoneInput;
