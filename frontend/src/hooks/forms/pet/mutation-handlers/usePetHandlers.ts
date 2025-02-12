c36f4baaf62a26610b604bce89365b7b8b169254
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2025-02-12 16:39:52 +0200

    feat: enhance usePetHandlers to support pet editing functionality

--- frontend/src/hooks/forms/pet/mutation-handlers/usePetHandlers.ts
+++ frontend/src/hooks/forms/pet/mutation-handlers/usePetHandlers.ts
@@ -2,8 +2,8 @@ import { useDialogState } from '@/hooks/useDialogState';
 import { usePetHook } from '../usePetHook';
 import { Pet } from '@/models/pet';
 
-export const usePetHandlers = () => {
-    const { useCreatePet } = usePetHook();
+export const usePetHandlers = (petId?: string) => {
+    const { useCreatePet, useEditPet } = usePetHook();
 
     const { isOpen, setIsOpen } = useDialogState();
 
@@ -12,12 +12,24 @@ export const usePetHandlers = () => {
         setIsOpen,
     });
 
+    const { mutate: editMutation, isPending: isPetEditPending } = useEditPet({
+        queryKey: ['userProfile'],
+        _id: petId,
+        setIsOpen,
+    });
+
     const handleCreatePet = (petData: Pet) => {
         createMutation(petData);
     };
 
+    const handleEditPet = (petData: Pet) => {
+        editMutation(petData);
+    };
+
     return {
         handleCreatePet,
+        handleEditPet,
+        isPetEditPending,
         isError,
         isOpen,
         setIsOpen,
